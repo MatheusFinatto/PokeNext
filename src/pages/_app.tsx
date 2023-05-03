86899472ba08d8982cc3c04797a2b20d2417708b
MatheusFinatto <matheusfinatto2@gmail.com>
2023-05-03 11:37:57 -0300

    build

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,15 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import Footer from "@/components/Footer";
+import Layout from "@/components/Layout";
+import Navbar from "@/components/Navbar";
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
+  );
 }
